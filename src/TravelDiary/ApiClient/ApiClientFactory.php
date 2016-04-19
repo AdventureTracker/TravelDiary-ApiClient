d6ea6a0df76c4dbaaa2887b9d33ef68e5e7826de
Jakub Dubec <jakub.dubec@gmail.com>
2016-04-19 15:32:18 +0200

    Init commit

---
+++ src/TravelDiary/ApiClient/ApiClientFactory.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jdubec
+ * Date: 19/04/16
+ * Time: 15:23
+ */
+
+class ApiClientFactory
+{
+
+	/**
+	 * @var string
+	 */
+	protected $baseUri;
+
+	/**
+	 * @var string
+	 */
+	protected $clientSecret;
+
+	/**
+	 * @var string
+	 */
+	protected $clientToken;
+
+	/**
+	 * ApiClientFactory constructor.
+	 * @param string $baseUri
+	 * @param string $clientSecret
+	 * @param string $clientToken
+	 */
+	public function __construct($baseUri, $clientSecret, $clientToken)
+	{
+		$this->baseUri = $baseUri;
+		$this->clientSecret = $clientSecret;
+		$this->clientToken = $clientToken;
+	}
+
+	/**
+	 * @param string $baseUri
+	 */
+	public function setBaseUri($baseUri)
+	{
+		$this->baseUri = $baseUri;
+	}
+
+	/**
+	 * @param string $clientSecret
+	 */
+	public function setClientSecret($clientSecret)
+	{
+		$this->clientSecret = $clientSecret;
+	}
+
+	/**
+	 * @param string $clientToken
+	 */
+	public function setClientToken($clientToken)
+	{
+		$this->clientToken = $clientToken;
+	}
+
+	public function create() {
+	}
+
+
+}
